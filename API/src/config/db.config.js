87d7ca2a4f362c6f9ce47ca60288b69eb69fa250
unknown <hector.murillo@coppel.com>
2024-12-30 20:27:47 -0700

    bd dev

--- API/src/config/db.config.js
+++ API/src/config/db.config.js
@@ -1,11 +1,11 @@
 // BD-LGT
-module.exports = {
-    HOST: `MYSQL8003.site4now.net`,
-    USER: `a55757_ltg`,
-    PASSWORD: `_3L3T3G3_`,
-    DB: `db_a55757_ltg`,
-    AUTH: false
-}
+// module.exports = {
+//     HOST: `MYSQL8003.site4now.net`,
+//     USER: `a55757_ltg`,
+//     PASSWORD: `_3L3T3G3_`,
+//     DB: `db_a55757_ltg`,
+//     AUTH: false
+// }
 
 //BD-TODOCHILO
 // module.exports = {
@@ -15,3 +15,13 @@ module.exports = {
 //     DB: `db_a55757_tc`,
 //     AUTH: false
 // }
+
+// BD-LGT DEV
+module.exports = {
+    HOST: `mysql5050.site4now.net`,
+    USER: `a55757_ltgdev`,
+    PASSWORD: `_3L3T3G3_DEV`,
+    DB: `db_a55757_ltgdev`,
+    AUTH: false
+}
+
